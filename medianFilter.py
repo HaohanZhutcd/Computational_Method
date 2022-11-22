bc5cbeddc6b0b8f96212d4b4fbaa145ffc101beb
HaohanZhutcd <zhuha@tcd.ie>
2022-11-22 16:07:44 +0000

    Update the pep8 Code Style

--- medianFilter.py
+++ medianFilter.py
@@ -1,8 +1,8 @@
-import numpy as np 
+# import numpy as np
 
 def medianFilter(signal_input, filter_len):
     if filter_len % 2 == 1:
-        pad_part =  (filter_len - 1) // 2
+        pad_part = (filter_len - 1) // 2
         filtered_Signal = []
         # print("pad_part:", pad_part)
         for i in range(len(signal_input)):
@@ -16,13 +16,13 @@ def medianFilter(signal_input, filter_len):
                 # print("remind:", int(filter_len - (pad_part - index_i + 1)))
                 for j in range(int(filter_len - (pad_part - index_i + 1))):
                     filter_window.append(signal_input[j])
-                # print("filter_window:", filter_window)    
+                # print("filter_window:", filter_window)
             elif ((index_i + pad_part) > len(signal_input)):
                 for k in range((i - pad_part), len(signal_input)):
                     # print("k:", k)
                     filter_window.append(signal_input[k])
                     # print("filter_window:", filter_window)
-                for l in range(i + pad_part - (len(signal_input) - 1)):
+                for p in range(i + pad_part - (len(signal_input) - 1)):
                     # print("l:", l)
                     filter_window.append(0)
                 # print("filter_window:", filter_window)
@@ -30,11 +30,9 @@ def medianFilter(signal_input, filter_len):
                 for z in range(filter_len):
                     # print("z:", z)
                     filter_window.append(signal_input[i + z - pad_part])
-                  
-            # print("filter_window:", filter_window) 
-            filter_window.sort()   
+            # print("filter_window:", filter_window)
+            filter_window.sort()
             # print("filter_window after sorted:", filter_window)
-            
             filtered_Signal.append(filter_window[pad_part])
         # print("filtered_Signal:", filtered_Signal)
         # print(len(filtered_Signal))
@@ -43,7 +41,7 @@ def medianFilter(signal_input, filter_len):
         print("Please replace the filter length in a odd number")
     else:
         print("Please check the filter length if it is integer number or not")
-    
+
 
 if __name__ == '__main__':
     signal = [1, 2, 3, 6, 10, 7, 2, 1]
--- medianFilter_test.py
+++ medianFilter_test.py
@@ -1,9 +1,10 @@
-import numpy as np 
+import numpy as np
 from medianFilter import medianFilter
+
 
 def medianFilter_numpuFunction_verification(signal_input, filter_len):
     if filter_len % 2 == 1:
-        pad_part =  (filter_len - 1) // 2
+        pad_part = (filter_len - 1) // 2
         filtered_Signal_test = []
         # print("pad_part:", pad_part)
         for i in range(len(signal_input)):
@@ -17,13 +18,13 @@ def medianFilter_numpuFunction_verification(signal_input, filter_len):
                 # print("remind:", int(filter_len - (pad_part - index_i + 1)))
                 for j in range(int(filter_len - (pad_part - index_i + 1))):
                     filter_window.append(signal_input[j])
-                # print("filter_window:", filter_window)    
+                # print("filter_window:", filter_window)
             elif ((index_i + pad_part) > len(signal_input)):
                 for k in range((i - pad_part), len(signal_input)):
                     # print("k:", k)
                     filter_window.append(signal_input[k])
                     # print("filter_window:", filter_window)
-                for l in range(i + pad_part - (len(signal_input) - 1)):
+                for p in range(i + pad_part - (len(signal_input) - 1)):
                     # print("l:", l)
                     filter_window.append(0)
                 # print("filter_window:", filter_window)
@@ -31,9 +32,8 @@ def medianFilter_numpuFunction_verification(signal_input, filter_len):
                 for z in range(filter_len):
                     # print("z:", z)
                     filter_window.append(signal_input[i + z - pad_part])
-            
             # print(np.median(filter_window))
-            # print("filter_window:", filter_window) 
+            # print("filter_window:", filter_window)
             filtered_Signal_test.append(int(np.median(filter_window)))
         # print("filtered_Signal:", filtered_Signal)
         # print(len(filtered_Signal))
@@ -43,13 +43,18 @@ def medianFilter_numpuFunction_verification(signal_input, filter_len):
     else:
         print("Please check the filter length if it is integer number or not")
 
+
 def compare_designed_and_given(my_design, numpy_result):
     if my_design == numpy_result:
-        print("The result of median filter designed by me is equal to the result of median funtion from numpy library")
+        print("The result of median filter designed by me is equal to \
+            the result of median funtion from numpy library")
         return True
     else:
-        print("The result of median filter designed by me is not equal to the result of median funtion from numpy library")
+        print("The result of median filter designed by me is not equal to \
+        the result of median funtion from numpy library")
         return False
+
+
 if __name__ == '__main__':
     signal = [1, 2, 3, 6, 10, 7, 2, 1]
     # print("The length of input data:", len(signal))
